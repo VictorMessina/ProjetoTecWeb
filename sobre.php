b4e74b7647873769fef072566dc0c2a1ad83784e
Patricia <Patricia@Patricia-VAIO>
2014-09-25 18:03:10 -0300

    nova pagina (sobre.php) e novo CSS (sobre.css)

---
+++ sobre.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<link rel="shortcut icon" type="image/x-icon" href="imagensSite/logo.png"/>
+		<title> Sobre Nós </title>
+		<meta charset="utf-8"/>
+		<meta name="descripition" content="Formulário"/>
+		<meta name="author" content="Bianca, Carol, Marcelo, Victor"/>
+                <link rel="stylesheet" type="text/css" href="css/sobre.css"/>
+                
+        </head>
+        
+            <body>
+
+                    <section>
+                        <p> <br/>
+                            O Site The North's Birds, é um site criado pelos alunos da FCI do proprio Mackenzie, para ajudar os alunos de todas as faculdades presentes dentro da universidade <br/> a realizar trocas, vendas e  
+                            compras de livros que não vão mais ser utilizados por seus donos. Este site surgiu da ideia de ajudar tanto os calouros quanto os veteranos da Universidade <br/> Presbiteriana Mackenzie, sendo assim
+                            para utilizar este site é necessario estar inscrito em algum <b> curso de nivel superior </b> dento da universidade, <b> ser maior de idade </b> e  <br/> <b> possuir um TIA (numero de indentificação do aluno)</b> para realizar o cadastro no site.
+                            <b> O site não se responsabiliza por qualquer problema que possa vir a acontecer entre as trocas, vendas ou compras então combine todos os detalhes com o fornecedor do livro ou dos livros que deseja adquirir. </b>
+                            <br/> 
+                            <br/>
+                             O site Agradece a sua visita. Obrigado, para voltar para pagina inicial do site clique na imagem abaixo.
+                            <br/>
+                        </p>
+                    </section>
+                
+                <div> 
+                    <img alt="logoMack" src="imagensSite/mackenzie.jpg" id='mackenzie'onclick= "javascript:window.location.href='index.php'"/> 
+                </div>
+                
+                <footer>
+                    <p>Copyright &copy; MackOnline 2014</p>
+                </footer>
+	
+            </body>
+	
+</html>
