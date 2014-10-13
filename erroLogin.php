3aa4398c6e3baba72de5acb1b2626b43f50708fb
Patricia <Patricia@Patricia-VAIO>
2014-10-13 19:41:44 -0300

    mudanças feitas na pagina erroLogin e criação da pagina css da mesma

--- erroLogin.php
+++ erroLogin.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>FORMS</title>
+		<title>Erro Login</title>
+                <link rel="shortcut icon" type="image/x-icon" href="imagensSite/logo.png"/>
 		<meta charset="utf-8">
+                <meta name="author" content="Bianca, Carol, Marcelo, Victor"/>
+                <link rel="stylesheet" type="text/css" href="css/erroLogin.css"/>
 	</head>
-	<body>
-		<h3> Email ou senha inválidos</h3>
-		<a href="index.php">VOLTAR</a>
-	</body>
+            <body>
+                <img alt="logoMack" src="imagensSite/mackenzie.jpg" id='mackenzie'/>
+                    <div>
+                        <h3> Email ou senha inválidos, volte e digite suas informações corretamente</h3>
+                        <a href="index.php">VOLTAR</a>
+                    </div>
+                        <footer>
+                            <p>Copyright &copy; MackOnline 2014</p>
+                        </footer>
+            </body>
 </html>
