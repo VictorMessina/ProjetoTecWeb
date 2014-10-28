34554fd981c8db5488a97fdcb0c1fc746b5fa65d
Victor Messina <>
2014-10-28 15:11:33 -0200

    validaLogin.php: adicionada funcionalidade para não deixar pessoas externas terem acesso a senha e o usuario das pessoas cadastradas no banco

--- validaLogin.php
+++ validaLogin.php
@@ -2,8 +2,8 @@
 
 include 'conectaDB.php';
 
- $email = $_POST['email'];
- $senha = $_POST['senha'];
+ $email = mysql_real_escape_string($_POST['email']);
+ $senha = mysql_real_escape_string($_POST['senha']);
  
   $sql = "SELECT * FROM cadastro WHERE(email='$email' && senha='$senha')";
                     $resultado = mysqli_query($conexao, $sql);
