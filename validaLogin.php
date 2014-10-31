c22848441b6d958dc7d8ce092dc45ef9911abe0e
31449530 <31449530@087865-P33S105.macklabs.br>
2014-10-31 09:25:05 -0200

    foi adicionada mais validações

--- validaLogin.php
+++ validaLogin.php
@@ -4,12 +4,11 @@ session_start();
 
 include 'conectaDB.php';
 
- $email = mysql_real_escape_string($_POST['email']);
- $senha = mysql_real_escape_string($_POST['senha']);
+ $email = $_POST['email'];
+ $senha = $_POST['senha'];
  
-  $sql = "SELECT * FROM cadastro WHERE(email='$email' && senha='$senha')";
+  $sql = 'SELECT * FROM cadastro WHERE email=\'$email\' and senha=\'$senha\'';
                     $resultado = mysqli_query($conexao, $sql);
-
                     if ($resultado){
                         $_SESSION["usuario"] = $email;
                         header("Location: perfil.php");
