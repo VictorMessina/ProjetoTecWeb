8b23336a02138c143e888528e8e49d0b53ad54cd
Marcelo <Marcelo@M-Desktop>
2014-11-05 19:50:41 -0200

    Php e Css do sobre e contato/  pagina sobre (finalizada)

--- sobre.php
+++ sobre.php
@@ -11,39 +11,37 @@
                 <script src="menu.js"></script>
 	</head>
 	
-	<body>
+    <body>
             
-            <nav>
-                <img alt="titulo" src="imagensSite/titulo.png" id='titulo'/>
+        <nav>
+            <img alt="titulo" src="imagensSite/titulo.png" id='titulo'/>
+        </nav>     
                 
-            </nav>     
-                
-            <header>
-                <?php include 'menu.php';?>
-            <header>
+        <header>
+            <?php include 'menu.php';?>
+        <header>
                 
                 
-                <div id="principal">
-                
-                    <section>
-                        <p style="text-align:justify; font-size:18pt;"> <br/>
-                            O Site The North's Birds, é um site criado pelos alunos da FCI do proprio Mackenzie, para ajudar os alunos de todas as faculdades presentes dentro da universidade <br/> a realizar trocas, vendas e  
-                            compras de livros que não vão mais ser utilizados por seus donos. Este site surgiu da ideia de ajudar tanto os calouros quanto os veteranos da Universidade <br/> Presbiteriana Mackenzie, sendo assim
-                            para utilizar este site é necessario estar inscrito em algum <b> curso de nivel superior </b> dento da universidade, <b> ser maior de idade </b> e  <br/> <b> possuir um TIA (numero de indentificação do aluno)</b> para realizar o cadastro no site.
-                            <b> O site não se responsabiliza por qualquer problema que possa vir a acontecer entre as trocas, vendas ou compras então combine todos os detalhes com o fornecedor do livro ou dos livros que deseja adquirir. </b>
-                            <br/> 
-                            <br/>
-                             O site Agradece a sua visita.
-                            <br/>
-                        </p>
-                    </section>
-                  
-                </div>
-                
-                <footer>
-                    <p>Copyright &copy; MackOnline 2014</p>
-                </footer>
+        <div id="principal">
+            <section>
+                <br/>
+                <p id="txt"> 
+                O Site The North's Birds, é um site criado pelos alunos da FCI do proprio Mackenzie, para ajudar os alunos de todas as faculdades presentes dentro da universidade <br/> a realizar trocas, vendas e  
+                compras de livros que não vão mais ser utilizados por seus donos. Este site surgiu da ideia de ajudar tanto os calouros quanto os veteranos da Universidade <br/> Presbiteriana Mackenzie, sendo assim
+                para utilizar este site é necessario estar inscrito em algum <b> curso de nivel superior </b> dento da universidade, <b> ser maior de idade </b> e  <br/> <b> possuir um TIA (numero de indentificação do aluno)</b> para realizar o cadastro no site.
+                <b> O site não se responsabiliza por qualquer problema que possa vir a acontecer entre as trocas, vendas ou compras então combine todos os detalhes com o fornecedor do livro ou dos livros que deseja adquirir. </b>
+                <br/> 
+                <br/>
+                O site Agradece a sua visita.
+                <br/>
+                </p>
+            </section>
+        </div>
+            
+        <footer>
+            <p>Copyright &copy; The North's Birds 2014</p>
+        </footer>
 	
-            </body>
+    </body>
 	
 </html>
