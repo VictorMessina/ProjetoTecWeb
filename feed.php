74c1157442330237a931c9d4d60fd63c333adcc8
Caroline <Caroline@Carol>
2014-11-05 22:35:28 -0200

    mudança no feed e criação do banco de dados das postagens

--- feed.php
+++ feed.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+    
+    <head>
+		<title>Bem-vindo!</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	</head>
+        
 	<head>
-		<link rel="shortcut icon" type="image/x-icon" href="imagensSite/logo.png"/>
 		<title>Feed</title>
 		<meta charset="utf-8"/>
 		<meta name="descripition" content="feed"/>
@@ -13,11 +19,25 @@
 	
 	<body>
             
-            <nav>
-                <img alt="titulo" src="imagensSite/titulo.png" id='titulo'/> 
-                <div id="principal">
-                    
-                    <?php
+            <nav>			
+			<img alt='titulo' src='img/titulo.png' id='titulo' />
+	    </nav>
+            
+            <div>
+			<?php
+				session_start();
+				
+				echo "<br/>";				
+				echo "Olá ".$_SESSION['nome'].", seja bem-vindo(a)!<br/>";
+			?>
+            
+                <br/><img alt='imagem' id='fotoPerfil' src="<?php echo $_SESSION['imagem']; ?>"/>
+				<form action ='createPostagem.php' method='POST' id='excecao'>
+				<p>Post:<p/><textarea name='post' rows='10' cols='50' maxlength='500'></textarea>
+				<p><input type='submit' value='PUBLICAR'/></p>
+				</form>
+                
+                <?php
                     
                         echo "<h3>". $nome=$_GET['nome']; "</h3>";
                         echo "<h3>Curso: ". $_POST['curso']; "</h3>";
@@ -31,16 +51,17 @@
                         echo "<br/><img alt='imagem' id='fotoLivro' src='$imagem' style='heigth:100px; width:150px; border-radius:10px; margin-left:30px;' />";
                         
                     ?>
-                    
+                
                 </div>
-            </nav>  
+   
             
             <header>
               <?php include 'menu.php';?>
             </header>
             
-                <footer>
-		    <p>Copyright &copy; MackOnline 2014</p>
-		</footer>	
+            <footer>
+			<?php echo "Você acessou como ".$_SESSION['nome']."."; ?> (<a href='index.php'><b>SAIR</b></a>)<br/>
+	        <p>Copyright &copy; MackOnline 2014</p>
+	    </footer>
 	</body>
 </html>
